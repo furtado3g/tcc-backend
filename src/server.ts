edaadb2abf3bbef1581106f08ef23b7a1de72819
Lucas Furtado <lucas_shiguioka@hotmail.com>
2020-11-05 20:22:41 -0300

    PORT

--- src/server.ts
+++ src/server.ts
@@ -11,6 +11,6 @@ app.use(express.json())
 app.use(cors())
 app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 app.use(routes)
-app.listen(port,()=>{
+app.listen(process.env.PORT||port,()=>{
     console.log("server started at port "+port)
 }) 
