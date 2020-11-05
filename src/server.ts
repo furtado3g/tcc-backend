37e34e1e03a075574684ab8429be09d153a3c3e8
Lucas Furtado <lucas_shiguioka@hotmail.com>
2020-11-05 20:20:27 -0300

    changed port

--- src/server.ts
+++ src/server.ts
@@ -11,6 +11,6 @@ app.use(express.json())
 app.use(cors())
 app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 app.use(routes)
-app.listen('https://rlab-backend.herokuapp.com/',()=>{
+app.listen(port,()=>{
     console.log("server started at port "+port)
 }) 
