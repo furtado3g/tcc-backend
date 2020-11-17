e5060337b3d5bb0745600ab4edf6955df7c65e36
Lucas Furtado <lucas_shiguioka@hotmail.com>
2020-11-17 14:04:10 -0300

    join

--- src/models/locationModel.ts
+++ src/models/locationModel.ts
@@ -72,7 +72,8 @@ class LocationModel {
     console.log(page)
     if (page !== null) {
       await db("locations")
-        .select("*")
+        .join('type_location','type_location.id','locations.type')
+        .select('type_location.desctiption','locations.id','locations.desctiption','locations.capacity')
         .where('disabled',false)
         .limit(perPage || 5)
         .offset((page * perPage )||0)
