48ae135a984cab6766c05f620887fcfb19cf8f81
Lucas Furtado <lucas_shiguioka@hotmail.com>
2020-11-19 18:39:35 -0300

    reserve

--- src/models/reserveModel.ts
+++ src/models/reserveModel.ts
@@ -1,97 +1,105 @@
 import db from "../database/connection";
-import moment from 'moment'
-import { attachPaginate } from 'knex-paginate' ;
+import moment from "moment";
+import { attachPaginate } from "knex-paginate";
 
-
-interface reserveInterface{
-    "teacher_id":number,
-    "location_id":number,
-    "date":string,
-    "time_start":string,
-    "time_end":string,
-    "class":string,
-    "discipline":string,
-    "comments":Text
+interface reserveInterface {
+  teacher_id: number;
+  location_id: number;
+  date: string;
+  time_start: string;
+  time_end: string;
+  class: string;
+  discipline: string;
+  comments: Text;
 }
 
-class ReserveModel{
-    format = 'HH:mm';
-    
-    async insert (reserve:reserveInterface){
-        let returnable
-        const labIsTaken = await db('reservations')
-        .where('location_id',reserve.location_id)
-        .where('date',moment(reserve.date))
-        .whereBetween('time_start',[moment(reserve.time_end,this.format),moment(reserve.time_start,this.format)])
-        .whereBetween('time_end',[moment(reserve.time_end,this.format),moment(reserve.time_start,this.format)])
-        if(labIsTaken[0]){
-            return {
-                message : "Espaço já reservado"
-            }
-        }
-        const insertedRows = await db('reservations').insert(reserve)
-        .then(data=>{
-            console.log(data)
-            returnable = {
-                message:"Reserva efetuada com sucesso"
-            }
-        })
-        .catch(e=>{
-            //traduzir retorno a baixo
-            returnable = {
-                error : "Erro ao realizar reserva"
-            }
-        })
-        return returnable
-    }
+class ReserveModel {
+  format = "HH:mm";
 
-    async update (reserve:reserveInterface,reserveId:number){
-        let returnable 
-        console.log(reserve)
-        const insertedRows = await db('reservations')
-        .where('id',reserveId)
-        .update(reserve)
-        .then(data=>{
-            returnable = {
-                message:"Reserva atualizada com sucesso"
-            }
-        }).catch(()=>{
-            returnable = {
-                error : "Erro ao atualizar reserva"
-            }
-        }) 
-        return returnable
+  async insert(reserve: reserveInterface) {
+    let returnable;
+    const labIsTaken = await db("reservations")
+      .where("location_id", reserve.location_id)
+      .where("date", moment(reserve.date))
+      .whereBetween("time_start", [
+        moment(reserve.time_end).format(this.format),
+        moment(reserve.time_start).format(this.format),
+      ])
+      .whereBetween("time_end", [
+        moment(reserve.time_end).format(this.format),
+        moment(reserve.time_start).format(this.format),
+      ]);
+    if (labIsTaken[0]) {
+      return {
+        message: "Espaço já reservado",
+      };
     }
+    const insertedRows = await db("reservations")
+      .insert(reserve)
+      .then((data) => {
+        console.log(data);
+        returnable = {
+          message: "Reserva efetuada com sucesso",
+        };
+      })
+      .catch((e) => {
+        //traduzir retorno a baixo
+        returnable = {
+          error: "Erro ao realizar reserva",
+        };
+      });
+    return returnable;
+  }
 
-    async delete(reserveId:any){
-        let returnable 
-        const deletedRows = await db('reservations')
-        .where('id','=',reserveId)
-        .delete()
-        .then(data=>{
-            returnable = {
-                message : "Reserva excluída com sucesso"
-            }
-        }).catch(e=>{
-            returnable = {
-                error : "Erro ao excluir reserva"
-            }
-        })
-        return returnable 
-    }
+  async update(reserve: reserveInterface, reserveId: number) {
+    let returnable;
+    console.log(reserve);
+    const insertedRows = await db("reservations")
+      .where("id", reserveId)
+      .update(reserve)
+      .then((data) => {
+        returnable = {
+          message: "Reserva atualizada com sucesso",
+        };
+      })
+      .catch(() => {
+        returnable = {
+          error: "Erro ao atualizar reserva",
+        };
+      });
+    return returnable;
+  }
 
-    async list(page:any,perPage:any){
-        const itens = await db('reservations')
-        .limit(perPage || 10)
-        .offset((page*perPage) || 0)
-        .select('*')
-        return itens
-    }
+  async delete(reserveId: any) {
+    let returnable;
+    const deletedRows = await db("reservations")
+      .where("id", "=", reserveId)
+      .delete()
+      .then((data) => {
+        returnable = {
+          message: "Reserva excluída com sucesso",
+        };
+      })
+      .catch((e) => {
+        returnable = {
+          error: "Erro ao excluir reserva",
+        };
+      });
+    return returnable;
+  }
 
-    async detail(reserveId:any){
-        const reserve = db('reservations').select('*').where('id',reserveId)
-        return reserve
-    }
+  async list(page: any, perPage: any) {
+    const itens = await db("reservations")
+      .limit(perPage || 10)
+      .offset(page * perPage || 0)
+      .select("*");
+    return itens;
+  }
+
+  async detail(reserveId: any) {
+    const reserve = db("reservations").select("*").where("id", reserveId);
+    return reserve;
+  }
 }
 
-export default ReserveModel
+export default ReserveModel;
