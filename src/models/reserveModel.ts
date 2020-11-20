1a71ebe775095e0cd31d4cf6dd10c20a1254f2b1
Lucas Furtado <lucas_shiguioka@hotmail.com>
2020-11-20 14:52:37 -0300

    dat error correction

--- src/models/reserveModel.ts
+++ src/models/reserveModel.ts
@@ -66,7 +66,7 @@ class ReserveModel {
       .update({
         teacher_id: reserve.teacher_id,
         location_id: reserve.location_id,
-        date: reserve.date,
+        date: moment(reserve.date).format('DD/MM/YYYY'),
         time_start: moment(reserve.time_start).format(this.format),
         time_end: moment(reserve.time_end).format(this.format),
         class: reserve.class,
