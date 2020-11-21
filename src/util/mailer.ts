cece3bf77afe59e9c6b7aea1b003f17ccc06e498
Lucas Furtado <lucas_shiguioka@hotmail.com>
2020-11-21 19:43:49 -0300

    email

--- src/util/mailer.ts
+++ src/util/mailer.ts
@@ -1,4 +1,5 @@
 import * as nodemailer from "nodemailer";
+import  sgTransport from 'nodemailer-sendgrid-transport'
 import * as config from '../configs/mail.json'
 class Mail {
 
@@ -17,14 +18,11 @@ class Mail {
             html: this.message
         };
 
-        const transporter = nodemailer.createTransport({
-            service: 'gmail',
+        const transporter = nodemailer.createTransport(sgTransport({
             auth: {
-                user: config.user,
-                pass: config.password
-            },
-            tls: { rejectUnauthorized: false }
-        });
+                api_key: config.api_key
+            }    
+        }));
 
         transporter.sendMail(mailOptions, (error : any, info:any) => {
             if (error) {
