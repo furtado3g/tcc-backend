8bd1c56ee7dc2d9cc052672ddcc37b50a3563c75
Lucas Furtado <lucas_shiguioka@hotmail.com>
2020-11-23 13:45:24 -0300

    .

--- src/controllers/reportsController.ts
+++ src/controllers/reportsController.ts
@@ -2,16 +2,16 @@ import { Request, Response } from "express";
 import Session from "../models/sessionModel";
 import Verify from "../util/verify";
 import reportsModel from "../models/reportsModel";
+const session = new Session();
+const verify = new Verify();
+const model = new reportsModel();
 class ReportsController {
-  session = new Session();
-  verify = new Verify();
-  model = new reportsModel();
   async perUser(req: Request, res: Response) {
     const { id } = req.params;
     const { begin, end }: any = req.query;
     const { authorization, userid } = req.headers;
     if (
-      !this.verify.verifyNullIncommingFields({
+      !verify.verifyNullIncommingFields({
         id,
         begin,
         end,
@@ -22,9 +22,9 @@ class ReportsController {
       return res
         .status(404)
         .json({ message: "Campo obrigatório não informado" });
-    const logged = await this.session.verify(authorization);
+    const logged = await session.verify(authorization);
     if (!logged.is_valid) res.status(404).json({ error: "Sessão inválida" });
-    const data = await this.model.perUser(id, begin, end);
+    const data = await model.perUser(id, begin, end);
     if(!data[0]){
         res.status(404).json({"error":"Não foi encontrada nenhuma ocorrencia na data"})
     }else{
