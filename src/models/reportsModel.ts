d3c6eec1679d16d93785262c293f4c8eb330d1b7
Lucas Furtado <lucas_shiguioka@hotmail.com>
2020-11-23 10:54:59 -0300

    created report to user

---
+++ src/models/reportsModel.ts
@@ -0,0 +1,27 @@
+import db from "../database/connection";
+import moment from "moment";
+class ReportsModel {
+  async perUser(userId: string, begin: string, end: string) {
+    return await db("reservations")
+      .where("reservations.teacher_id", userId)
+      .whereBetween("reservations.date", [
+        moment(begin, "DD/MM/RRRR"),
+        moment(end, "DD/MM/RRRR"),
+      ])
+      .join("locations", "locations.id", "reservations.location_id")
+      .select(
+        "loc.comments as localName",
+        "res.date as date",
+        "res.time_start as start",
+        "res.time_end as end",
+        "res.class as class",
+        "res.discipline as discipline",
+        "res.comments as observations"
+      )
+      .then((data) => {
+        return data;
+      });
+  }
+}
+
+export default ReportsModel
--- src/router.ts
+++ src/router.ts
@@ -6,53 +6,57 @@ import LocationController from "./controllers/locationController";
 import PermissionController from "./controllers/permissionController";
 import LocationUserController from "./controllers/locationUserController";
 import TypeLocationController from "./controllers/TypeLocationController";
+import ReportsController from "./controllers/reportsController";
 
 const routes = express.Router();
-//inicio das controllers 
-const users = new userController()
-const sessions = new sessionController()
-const reserves = new ReserveController()
-const location = new LocationController()
-const permission = new PermissionController()
-const locationUser = new LocationUserController()
-const locationType = new TypeLocationController()
+//inicio das controllers
+const users = new userController();
+const sessions = new sessionController();
+const reserves = new ReserveController();
+const location = new LocationController();
+const permission = new PermissionController();
+const locationUser = new LocationUserController();
+const locationType = new TypeLocationController();
+const reports = new ReportsController();
 //controle  de sessão
-routes.post('/session',users.validate)
-routes.put('/session',sessions.extendSession)
+routes.post("/session", users.validate);
+routes.put("/session", sessions.extendSession);
 //controle de usuarios
-routes.post('/user',users.create)
-routes.put('/user',users.update)
-routes.get('/user/:id',users.detail)
-routes.delete('/user/:id',users.disableUser)
-routes.get('/users',users.listUsers)
-routes.post('/recovery',users.recoveryPassword)
-routes.put('/user/changePassword',users.updatePassword)
+routes.post("/user", users.create);
+routes.put("/user", users.update);
+routes.get("/user/:id", users.detail);
+routes.delete("/user/:id", users.disableUser);
+routes.get("/users", users.listUsers);
+routes.post("/recovery", users.recoveryPassword);
+routes.put("/user/changePassword", users.updatePassword);
 //controle de tipo de local
-routes.post('/location/type/',locationType.create)
-routes.get('/location/type/',locationType.list)
-routes.delete('/location/type/:id',locationType.delete)
-routes.get('/location/type/:id',locationType.detaiL)
-routes.put('/location/type/:id',locationType.edit)
-//controle de reservas 
-routes.post('/reserve/',reserves.create)
-routes.put('/reserve/:reserveId',reserves.update)
-routes.get('/reserve/',reserves.list)
-routes.get('/reserve/:reserveId',reserves.detail)
-routes.delete('/reserve/:reserveId',reserves.delete)
+routes.post("/location/type/", locationType.create);
+routes.get("/location/type/", locationType.list);
+routes.delete("/location/type/:id", locationType.delete);
+routes.get("/location/type/:id", locationType.detaiL);
+routes.put("/location/type/:id", locationType.edit);
+//controle de reservas
+routes.post("/reserve/", reserves.create);
+routes.put("/reserve/:reserveId", reserves.update);
+routes.get("/reserve/", reserves.list);
+routes.get("/reserve/:reserveId", reserves.detail);
+routes.delete("/reserve/:reserveId", reserves.delete);
 //controle de responsaveis do local
-routes.post('/location/user/',locationUser.assign)
-routes.delete('/location/user/:locationUserId',locationUser.unassign)
-routes.get('/location/:locationId/user/:id',locationUser.unassign)
-routes.get('/location/user/:id',locationUser.listAssigns)
+routes.post("/location/user/", locationUser.assign);
+routes.delete("/location/user/:locationUserId", locationUser.unassign);
+routes.get("/location/:locationId/user/:id", locationUser.unassign);
+routes.get("/location/user/:id", locationUser.listAssigns);
 //controle de locais
-routes.post('/location/',location.new)
-routes.put('/location/:locationId',location.update)
-routes.delete('/location/:locationId',location.delete)
-routes.get('/locations',location.list)
-routes.get('/location/:locationId',location.detail)
+routes.post("/location/", location.new);
+routes.put("/location/:locationId", location.update);
+routes.delete("/location/:locationId", location.delete);
+routes.get("/locations", location.list);
+routes.get("/location/:locationId", location.detail);
 //controle de permissões
-routes.post('/endpoint',permission.newEndpoint)
-routes.post('/usertype',permission.newUserType)
-routes.post('/permission',permission.assignPermission)
-routes.get('/user/:idUser/permissions',permission.listUserPermissions)
-export default routes
+routes.post("/endpoint", permission.newEndpoint);
+routes.post("/usertype", permission.newUserType);
+routes.post("/permission", permission.assignPermission);
+routes.get("/user/:idUser/permissions", permission.listUserPermissions);
+//relatorios
+routes.get("reports/user/:id",reports.perUser);
+export default routes;
